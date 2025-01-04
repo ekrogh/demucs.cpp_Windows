69daaa9efd3a00e5d783ac1f619aaa32cefb8dd5
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2025-01-04 22:23:55 +0100

    getWeights.bat

---
+++ getWeights.bat
@@ -0,0 +1,6 @@
+python ./scripts/convert-pth-to-ggml.py --six-source ./ggml-demucs
+python ./scripts/convert-pth-to-ggml.py --ft-drums ./ggml-demucs
+python ./scripts/convert-pth-to-ggml.py --ft-vocals ./ggml-demucs
+python ./scripts/convert-pth-to-ggml.py --ft-bass ./ggml-demucs
+python ./scripts/convert-pth-to-ggml.py --ft-other ./ggml-demucs
+python ./scripts/convert-pth-to-ggml.py --v3 ./ggml-demucs
